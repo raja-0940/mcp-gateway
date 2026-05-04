87af65a161e847e2a670baee2ec661b8d4475ded
Rajakumar Battula <rbattula@redhat.com>
2026-05-04 09:52:25 +0530

    MULTIARCH-5995: add ppc64le support to the build
    Signed-off-by: Rajakumar Battula <rbattula@redhat.com>

--- build/istio.mk
+++ build/istio.mk
@@ -1,34 +1,59 @@
 # Istio
 
-SAIL_VERSION = 1.27.0
-ISTIO_NAMESPACE = istio-system
-ISTIO_VERSION = 1.27.0
+ARCH ?= $(shell uname -m)
+SAIL_VERSION ?= 1.27.0
+ISTIO_NAMESPACE ?= istio-system
+ISTIO_VERSION ?= 1.26.3
+
+TIMEOUT ?= 900s
 
 # istioctl tool
 ISTIOCTL = bin/istioctl
+ISTIO_SRC_DIR = /tmp/istio-src-$(ISTIO_VERSION)
+
+
+ISTIO_MANIFEST ?= config/istio/istio.yaml
+
+ifeq ($(ARCH),ppc64le)
+ISTIO_MANIFEST := config/istio/istio.ppc64le.yaml
+endif
+
+
 $(ISTIOCTL):
 	mkdir -p bin
-	curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=$(ISTIO_VERSION) TARGET_ARCH=$(ARCH) sh -
-	mv istio-$(ISTIO_VERSION)/bin/istioctl bin/
-	rm -rf istio-$(ISTIO_VERSION)
+	@if [ "$(ARCH)" = "ppc64le" ]; then \
+		echo "Building istioctl $(ISTIO_VERSION) for $(ARCH)..."; \
+          	rm -rf "$(ISTIO_SRC_DIR)"; \
+        	git clone --depth 1 --branch "$(ISTIO_VERSION)" https://github.com/istio/istio.git "$(ISTIO_SRC_DIR)"; \
+        	cd "$(ISTIO_SRC_DIR)" && \
+            	GOOS=linux GOARCH=$(ARCH) CGO_ENABLED=0 go build -o "$(CURDIR)/$(ISTIOCTL)" ./istioctl/cmd/istioctl; \
+        	rm -rf "$(ISTIO_SRC_DIR)"; \
+   	 else \
+        	echo "Downloading istioctl $(ISTIO_VERSION) for $(ARCH)..."; \
+        	curl -sL https://istio.io/downloadIstio | ISTIO_VERSION=$(ISTIO_VERSION) TARGET_ARCH=$(ARCH) sh -; \
+       		mv "istio-$(ISTIO_VERSION)/bin/istioctl" bin/; \
+       		rm -rf "istio-$(ISTIO_VERSION)"; \
+   	 fi
 
+.PHONY: istioctl-impl
 istioctl-impl: $(ISTIOCTL)
 	@echo "istioctl installed at: $(ISTIOCTL)"
 	@echo "Version: $$($(ISTIOCTL) version --remote=false)"
 
 .PHONY: istio-install
-istio-install: $(HELM) # Install Istio using Sail operator
+istio-install: $(HELM) ## Install Istio using Sail operator
 	$(HELM) upgrade --install sail-operator \
 		--create-namespace \
-		--namespace $(ISTIO_NAMESPACE) \
-		--wait \
-		--timeout=300s \
-		https://github.com/istio-ecosystem/sail-operator/releases/download/$(SAIL_VERSION)/sail-operator-$(SAIL_VERSION).tgz
-	kubectl apply -f config/istio/istio.yaml
-	kubectl -n $(ISTIO_NAMESPACE) wait --for=condition=Ready istio/default --timeout=300s
+        	--namespace $(ISTIO_NAMESPACE) \
+        	--wait \
+        	--timeout=$(TIMEOUT) \
+        	https://github.com/istio-ecosystem/sail-operator/releases/download/$(SAIL_VERSION)/sail-operator-$(SAIL_VERSION).tgz
+	kubectl apply -f $(ISTIO_MANIFEST)
+	kubectl -n $(ISTIO_NAMESPACE) wait --for=condition=Ready istio/default --timeout=$(TIMEOUT)
 
 .PHONY: istio-uninstall
-istio-uninstall: $(HELM) # Uninstall Istio and Sail operator
-	- kubectl delete -f config/istio/istio.yaml
+istio-uninstall: $(HELM) ## Uninstall Istio and Sail operator
+	- kubectl delete -f $(ISTIO_MANIFEST)
 	$(HELM) uninstall sail-operator -n $(ISTIO_NAMESPACE)
 	- kubectl delete namespace $(ISTIO_NAMESPACE)
+
--- build/metallb.mk
+++ build/metallb.mk
@@ -5,6 +5,7 @@ METALLB_VERSION = v0.15.2
 .PHONY: metallb-install
 metallb-install: $(YQ) # Install MetalLB load balancer
 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$(METALLB_VERSION)/config/manifests/metallb-native.yaml
+	kubectl -n metallb-system create secret generic memberlist --from-literal=secretkey="$$(openssl rand -base64 128)" --dry-run=client -o yaml | kubectl apply -f -
 	kubectl -n metallb-system wait --for=condition=Available deployments controller --timeout=300s
 	kubectl -n metallb-system wait --for=condition=ready pod --selector=app=metallb --timeout=120s
 	./utils/docker-network-ipaddresspool.sh kind $(YQ) | kubectl apply -n metallb-system -f -
